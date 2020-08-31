4b3c46525c089877f84d175d1297ee3264d2ed0c
Franco Narvaez <jnarvaez@est.frba.utn.edu.ar>
2020-08-31 17:40:18 -0300

    Se prueba la API CoinCap
    Se instaló TailwindCSS junto con sus respectivas
    configuraciones.
    Además, luego se instanció el llamado a la Api de
    CoinCap en Api.js para luego mostrar por pantalla
    los assets (criptocurrency), en App.vue
    
    No se realizaron más tareas

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <table class="table-auto">
+            <thead>
+                <tr>
+                    <th class="px-4 py-2"></th>
+                    <th class="px-4 py-2">Ranking</th>
+                    <th class="px-4 py-2">Nombre</th>
+                    <th class="px-4 py-2">Precio</th>
+                    <th class="px-4 py-2">Cap. de Mercado</th>
+                    <th class="px-4 py-2">Variación 24hs</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="a in assets" :key="a.id">
+                    <td>
+                        <img
+                            class="h-16 w-16"
+                            :src="
+                                `https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`
+                            "
+                            :alt="a.name"
+                        />
+                    </td>
+                    <td># {{ a.rank }}</td>
+                    <td>{{ a.name }}</td>
+                    <td>{{ Math.trunc(a.priceUsd) }}</td>
+                    <td>{{ Math.trunc(a.marketCapUsd) }}</td>
+                    <td>{{ Math.trunc(a.changePercent24Hr) }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import api from '@/api';
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data() {
+        return {
+            assets: [],
+        };
+    },
+    created() {
+        api.getAssets().then((assets) => (this.assets = assets));
+    },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
